d9a72e0399bd868291629d8688fdeeb28e96987e
Karim Ouazzou <karimouazzou@gmail.com>
2023-10-07 20:33:07 +0100

    creating student using params

--- src/main/java/com/springboot/app/controller/StudentController.java
+++ src/main/java/com/springboot/app/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.app.model.Student;
@@ -30,6 +31,11 @@ public class StudentController {
 	
 	@GetMapping("/student/{fn}/{ln}")
 	public Student getDynamicStudent(@PathVariable("fn") String firstName,@PathVariable("ln") String lastName) {
+		return new Student(firstName,lastName);
+	}
+	
+	@GetMapping("/student/query")
+	public Student studentQueryParam(@RequestParam(name="fn") String firstName,@RequestParam(name="ln") String lastName) {
 		return new Student(firstName,lastName);
 	}
 }
