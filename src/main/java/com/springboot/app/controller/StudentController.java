1ad203a08731d389df025c0795ad1819e51600f4
Karim Ouazzou <karimouazzou@gmail.com>
2023-10-07 20:18:35 +0100

    show list of students

--- src/main/java/com/springboot/app/controller/StudentController.java
+++ src/main/java/com/springboot/app/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.springboot.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,20 @@ import com.springboot.app.model.Student;
 
 @RestController
 public class StudentController {
+	
 	@GetMapping("/student")
 	public Student getStudent() {
 		return new Student("Karim", "Ouazzou");
+	}
+	
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+		students.add(new Student("FN1", "FN1"));
+		students.add(new Student("FN2", "FN2"));
+		students.add(new Student("FN3", "FN3"));
+		
+		return students;
 	}
 }
